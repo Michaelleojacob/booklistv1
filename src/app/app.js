707c951988b2090e464c7919d74a85eef63b4db4
Michael <michaelleojacob@gmail.com>
2021-07-27 16:45:45 -0700

    making an alertarea class to hold if there are alert messages. Such as a succesfull message for successfully adding a book, or a danger alert for if there are empty fields when making a book.

--- src/app/app.js
+++ src/app/app.js
@@ -8,7 +8,6 @@ export default function myapp() {
 			this.pages = pages;
 		}
 	}
-
 	//UI class: handle UI tasks
 	class UI {
 		static displayBooks() {
@@ -24,36 +23,57 @@ export default function myapp() {
 					pages: '75',
 				},
 			];
-
 			const books = StoredBooks;
-
 			books.forEach(book => UI.addBookToList(book));
 		}
 		static addBookToList(book) {
 			const list = document.querySelector('#book-list');
-
 			const row = document.createElement('tr');
-
 			row.innerHTML = `
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.pages}</td>
         <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>`;
-
 			list.appendChild(row);
 		}
+		static deleteBook(targetEl) {
+			if (targetEl.classList.contains('delete')) {
+				targetEl.parentElement.parentElement.remove();
+			}
+		}
+
 		static clearFields() {
 			document.querySelector('#title').value = '';
 			document.querySelector('#author').value = '';
 			document.querySelector('#pages').value = '';
 		}
 	}
-
+	class AlertArea {
+		static makeAlertDivArea() {
+			this.alertDiv = document.createElement('div');
+			const form = document.querySelector('#book-form');
+			form.prepend(this.alertDiv);
+		}
+		static clearText() {
+			this.alertDiv.textContent = '';
+		}
+		static removeAlertAndText() {
+			this.alertDiv.className = '';
+			this.alertDiv.textContent = '';
+		}
+		static showAlertAndAddText(str, info) {
+			this.alertDiv.classList.add('alert', `alert-${info}`);
+			this.alertDiv.textContent = str;
+			// setTimeout(() => {
+			// 	AlertArea.removeAlertAndText();
+			// }, 3000);
+		}
+	}
+	AlertArea.makeAlertDivArea();
+	AlertArea.showAlertAndAddText('hi', 'success');
 	//Store class: handles Storage
-
 	//event: display Books
 	document.addEventListener('DOMContentLoaded', UI.displayBooks);
-
 	//event: add a Book
 	document.querySelector('#book-form').addEventListener('submit', e => {
 		e.preventDefault();
@@ -61,13 +81,23 @@ export default function myapp() {
 		const title = document.querySelector('#title').value;
 		const author = document.querySelector('#author').value;
 		const pages = document.querySelector('#pages').value;
-		//instantiate book
-		const book = new Book(title, author, pages);
-		//add book to ui
-		UI.addBookToList(book);
-		//clear fields
-		UI.clearFields();
+		//validate
+		if (title === '' || author === '' || pages === '') {
+			return AlertArea.showAlertAndAddText(
+				'Please fill in all fields',
+				'danger'
+			);
+		} else {
+			//instantiate book
+			const book = new Book(title, author, pages);
+			//add book to ui
+			UI.addBookToList(book);
+			//clear fields
+			UI.clearFields();
+		}
 	});
-
 	//event:remove a Book
+	document.querySelector('#book-list').addEventListener('click', e => {
+		UI.deleteBook(e.target);
+	});
 }
